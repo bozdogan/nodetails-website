9d777a801024f54fbb6283f0c0577bf4920a46d7
bozdogan <boraozdogan99@gmail.com>
2021-06-11 22:54:06 +0300

    separated from backend code

--- app.py
+++ app.py
@@ -1,42 +1,38 @@
 import os.path as osp
 from flask import Flask, render_template, request
+import requests
 
-# try:
-    # import nodetails as nd
-    # from nodetails import ndabs, ndext
-# except ImportError:
-    # print("nodetails module is not found in the path")
-    # quit(1)
+API_URL = "http://httpbin.org/post"
+API_URL = "http://127.0.0.1:5000/sum"
 
-# import tensorflow.keras.backend as K; K.clear_session()
-# nd.enable_vram_growth()
+def call_api(api_url, text_body, method, model_name):
+    data = {"text": text_body, 
+            "method": method,
+            "model_name": model_name}
+    response = requests.post(api_url, data=data)
+    
+    if response.status_code == 200:
+        return response.json()["summary"]
+    else:
+        print("\n\n===RESPONSE===\n"+str(response.text)+"\n")
+
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    # if request.method == "POST":
-        # text_body = request.form["Inputtext"]
-        # sum_choice = request.form["sumChoice"]
-        # if sum_choice == "ext":
-            # summary = ndext.get_summary(text_body, length=7).summary
+    if request.method == "POST":
+        text_body = request.form["text"]
+        method = request.form["method"]
+        model_name = request.form["model_name"]
 
-            # print(ndext.get_summary(text_body, length=7))
-        # elif sum_choice == "abs":
-            # summary = ndabs.make_inference(model, text_body)
-        # else:
-            # print("HATA, HATA, HATA!", sum_choice)
+        summary = call_api(API_URL, text_body, method, model_name)
 
-        # return render_template(
-            # "index.html", text=text_body, summary=summary, choice=sum_choice)
-    # else:
-        # return render_template("index.html")
-    return render_template("index.html")
+        return render_template(
+            "index.html", text=text_body, summary=summary, choice=method)
+    else:
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
-    # model_dir = "../data/_models"
-    # model_name = "nodetails--food_reviews--80-10--None.model"
-    # model = ndabs.load_model(osp.join(model_dir, model_name))
-
     app.run(port=5001, debug=True)
