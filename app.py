9c4332b19957d0d5a3d35d9e5b22f855498ee078
bozdogan <boraozdogan99@gmail.com>
2021-06-19 23:02:11 +0300

    websiteee!!1

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+import sys
 from flask import Flask, render_template, request
 import requests
+import json
 
 def call_api(api_url, data):
     response = requests.post(api_url, data=data)
-    
     if response.status_code == 200:
-        return response.json()["summary"]
+        return response.json()
     else:
         print("\n\n===RESPONSE===\n"+str(response.text)+"\n")
 
@@ -16,14 +17,47 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         data = request.form
-        print(type(request.form))
-        summary = call_api("http://127.0.0.1:5000/sum", data)
-        summary = call_api("http://127.0.0.1:5000/sum", data)
+        response = call_api(API_URL + "/sum", data)
 
-        return render_template("index.html", summary=summary)
+        text = ""
+        text_display = ""
+        summary = ""
+        extended_summary = ""
+
+        if response:
+            if "reference" in response:
+                text_display = []
+                for k, v in response["sentences"].items():
+                    text_display.append(f'<a class="referenced" name="ref{k}">{v}</a>')
+                text_display = " ".join(text_display)
+
+                sum_linked = []
+                for ref in response["reference"]:
+                    sum_linked.append(f'<a href="#ref{ref}">{response["sentences"][ref]}</a>')
+                extended_summary = " ".join(sum_linked)
+            elif "summary" in response:
+                summary = response["summary"]
+                
+            # if "extended_summary" in response:
+            #     extended_summary = response["extended_summary"]
+
+
+            text = data["text"]
+            text = json.dumps(response)
+        else:
+            text = "Server unavailable"
+
+        return render_template("index.html",
+                               text=text,
+                               summary=summary,
+                               extended_summary=extended_summary,
+                               text_display=text_display)
     else:
         return render_template("index.html")
 
 
 if __name__ == "__main__":
+    API_URL = sys.argv[1] if len(sys.argv) > 1 else "http://127.0.0.1:5000"
+    print("API URL: %s\n" % API_URL)
+    
     app.run(port=5001, debug=True)
---
+++ static/js/betik.js
@@ -0,0 +1,18 @@
+var mybutton = document.getElementById("backtotop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 215;
+  document.documentElement.scrollTop = 215;
+}
